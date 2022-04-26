4e516e977ef91986d3e0e39e34788e704502749e
Veronika Háková <veronika.hakova@gmail.com>
2022-04-26 20:45:38 +0200

    funguje ukládání do localStorage

--- app.js
+++ app.js
@@ -7,22 +7,18 @@ by se měl seznam receptů vyfiltrovat podle hledaného slova.
 3) Doplň filtrovanání receptů podle kategorie.
 
 4) Doplň řazení receptů podle hodnocení.
-
-5) Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
-Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
-recept-hodnoceni, recept-nazev, recept-popis.
-
-6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
 
 //pole s recepty => recepty
 
 i = 0;
+localStorage = [];
 
 window.addEventListener("load", poNacteni);
 
 function poNacteni() {
     vypisRecepty();
+    alzheimerPojistka();
 }
 
 //Vytvoření náhledového boxíku receptu
@@ -30,20 +26,24 @@ function vypisRecepty() {
     for (let i=0; i<recepty.length; i++) {
         const poleReceptu = document.querySelector('#recepty');
 
+        //Generování struktury náhledového boxíku
         const boxReceptu = document.createElement('div');
         const boxObrazkuReceptu = document.createElement('div');
         const obrazekReceptu = document.createElement('img');
         const receptInfo = document.createElement('div');
         const receptNadpis = document.createElement('h3');
 
+        //Nastavení atributů
         boxReceptu.className = "recept";
         boxObrazkuReceptu.className = "recept-obrazek";
         receptInfo.className = "recept-info";
         boxReceptu.dataset.index = i;
 
+        //Generování obrázku a nadpisu
         obrazekReceptu.src = recepty[i].img;
         obrazekReceptu.alt = recepty[i].nadpis;
 
+        //Vypsání položek
         receptNadpis.innerHTML = recepty[i].nadpis;
 
         poleReceptu.appendChild(boxReceptu);
@@ -52,21 +52,39 @@ function vypisRecepty() {
         boxObrazkuReceptu.appendChild(obrazekReceptu);
         receptInfo.appendChild(receptNadpis);
 
+        //Při kliknutí zavolej vypisPopis()
         boxReceptu.addEventListener('click', vypisPopis);
     }
 
 }
 
 //Vytvoření detailního popisu ke zvolenému receptu
-
 function vypisPopis(vybranyRecept) {
+    //Zaměření indexu kliknutého políčka podle data-index = i
     let index = vybranyRecept.currentTarget.dataset.index;
 
+    vytvorSablonuPrispevku();
+
+    //Vymazání předchozího záznamu v localStorage
+    localStorage.clear();
+    //Zapsání posledního receptu do localStorage
+    localStorage.setItem("index", index);
+}
+
+//Vytvoření šablony pro detailní příspěvek
+function vytvorSablonuPrispevku() {
     document.querySelector('#recept-foto').src = recepty[index].img;
     document.querySelector('#recept-kategorie').innerHTML = recepty[index].kategorie;
     document.querySelector('#recept-hodnoceni').innerHTML = recepty[index].hodnoceni;
     document.querySelector('#recept-nazev').innerHTML = recepty[index].nadpis;
     document.querySelector('#recept-popis').innerHTML = recepty[index].popis;
+}
+
+//Vypsání posledního příspěvku z localStorage
+function alzheimerPojistka() {
+    index = localStorage.getItem("index");
+
+    vytvorSablonuPrispevku();
 }
 
 //Vyhledávání receptu
